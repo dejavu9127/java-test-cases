746c89509a0d8147c2ca889d7d2b09ac942d15dd
dejavu9127 <ani9127@gmail.com>
2016-12-24 07:43:06 +0100

    HelloWorld App

---
+++ HelloWorld/src/main/java/com/mycodes/samples/HelloApp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycodes.samples;
+
+/**
+ *
+ * @author dejavu
+ */
+public class HelloApp {
+    public static void main(String[] args){
+        Greetings person1=new Greetings();
+        person1.sayHello();
+    }
+}
